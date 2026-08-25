885d9b9bfcbd65b47bc395d06bee60c83c62ac16
Caradophp <contatolucianofriebe@gmail.com>
2026-08-25 20:02:58 -0300

    Crição dos projetos

---
+++ backend/smartRoute/src/main/java/com/faesa/smartRoute/SmartRouteApplication.java
@@ -0,0 +1,13 @@
+package com.faesa.smartRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRouteApplication.class, args);
+	}
+
+}
---
+++ backend/smartRoute/src/test/java/com/faesa/smartRoute/SmartRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faesa.smartRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
